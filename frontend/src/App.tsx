ca1c7de940855558562ae018afdde4cf0edbaf09
Elizabete <elizabete958@gmail.com>
2024-06-10 17:31:52 -0300

    Instalação de Material Ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@mui/material";
+
 export function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold mb-4">Hello Liza</h1>
+      <Button variant="contained" color="primary">
+        Material-UI Button
+      </Button>
     </div>
   );
 }
