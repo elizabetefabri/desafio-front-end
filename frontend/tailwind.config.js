1c854572de8cfb92978ed1d928257650855e9ac1
Elizabete <elizabete958@gmail.com>
2024-06-11 16:51:02 -0300

    configuration: configuração universal das cores em tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,40 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#3498db',
+        'secondary': '#2ecc71',
+        'accent': '#e74c3c',
+        'background': '#F9FAFB',
+        'white': '#ffffff',
+        'text': '#151D48',
+
+        'success-light': '#d3fcd2',
+        'success': '#22c55e',
+
+        'grey-light': '#eceff1',
+        'purple-light': '#efd6ff',
+        'grey-bg': '#f4f6f8',
+        'orange-dark': '#f57c00',
+        'blue-dark': '#1976d2',
+        'pink': '#fa5a7d',
+        'orange': '#ffab00',
+        'yellow-light': '#ffd666',
+        'pink-light': '#ffe2e5',
+        'yellow-bg': '#fff5cc',
+        'white-bg': '#ffffff',
+        'red-dark': '#ff5630',
+        'grey-dark': '#212b36',
+
+        'blue': '#2500ff',
+        'dark-grey': '#373d3f',
+
+        'grey': '#637381',
+        'black-transparent': 'rgba(0, 0, 0, 0%)',
+        'grey-opacity': 'rgba(145, 158, 171, 8%)',
+      },
+    },
   },
   plugins: [],
 }
