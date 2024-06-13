fedd4190bfcd9a01b5ca4c283fedcb2988fa318f
Elizabete <elizabete958@gmail.com>
2024-06-13 20:50:14 -0300

    otimização do componente

--- frontend/src/Components/Chart/AllProductsChart/index.tsx
+++ frontend/src/Components/Chart/AllProductsChart/index.tsx
@@ -1,67 +1,60 @@
-import React from "react";
-import { AllProductsData } from "../../../data/dados";
-import { GraphPercentage } from "./percentage";
+import './style.css';
 
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import React from 'react';
 
+import { AllProductsData } from '../../../data/dados';
+import GraphBar from './graph';
+import { GraphPercentage } from './percentage';
 
-export const AllProductsChart: React.FC = () => {
+const AllProductsChart: React.FC = () => {
   return (
-    <div className="shadow-md mt-6 p-4 rounded h-[300px] w-[550px] bg-white">
-      <div className="flex w-full mb-2 text-13 font-bold">
-        <h1 className="text-lg font-bold text-text mb-4">Todos Produtos</h1>
-      </div>
-      <div className="w-full h-[90] overflow-x-auto">
-        <table className="w-full table-auto border-collapse text-[10px]">
-          <thead>
-            <tr>
-              {AllProductsData.data.datasets.map((dataset) => (
-                <th
-                  key={dataset.label}
-                  className="px-4 py-2 font-normal text-[#94A3B8] text-left"
-                >
-                  {dataset.label}
-                </th>
-              ))}
-            </tr>
-          </thead>
+    <div className="w-full h-[300px] lg:w-[550px] bg-white rounded">
+      <TableContainer
+      component={Paper}
+      sx={{
+        overflow: "hidden",
+        height: '300px',
+      }}>
+      <h1 className="pl-4 font-bold text-text pt-2 ">Top Produtos</h1>
+      <Table stickyHeader aria-label="tabela de todos os produtos">
+        <TableHead>
+          <TableRow>
+            {AllProductsData.data.datasets.map((dataset) => (
+              <TableCell  style={{ fontWeight: 'normal', color: '#94A3B8' }}>
+                {dataset.label}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
           {AllProductsData.data.datasets.map((dataset, index) => (
-            <tbody key={index}>
-              <tr>
-                <td
-                  className={`px-4 ${dataset.customPadding} border-t border-gray-200 text-[#3F3F3F]`}
-                >
-                  {dataset.index}
-                </td>
-                <td
-                  className={`px-4 ${dataset.customPadding} border-t border-gray-200 uppercase text-[#3F3F3F]`}
-                >
-                  {dataset.product}
-                </td>
-                <td
-                  className={`px-4 ${dataset.customPadding} border-t border-gray-200`}
-                >
-                  <GraphPercentage
-                    percentageData={dataset.percentage}
-                    borderColor={dataset.graphPercentageBorder}
-                    background={dataset.graphPercentageBackground}
-                    textColor={dataset.percentageTextColor}
-                  />
-                </td>
-                <td
-                  className={`px-4 ${dataset.customPadding} border-t border-gray-200`}
-                >
-                  <GraphPercentage
-                    percentageData={dataset.percentage}
-                    borderColor={dataset.graphPercentageBorder}
-                    background={dataset.graphPercentageBackground}
-                    textColor={dataset.percentageTextColor}
-                  />
-                </td>
-              </tr>
-            </tbody>
+            <TableRow key={index}>
+              <TableCell>{dataset.index}</TableCell>
+              <TableCell>{dataset.product}</TableCell>
+              <TableCell>
+                <GraphBar
+                  percentageData={dataset.percentage}
+                  graphBarColor={dataset.graphBarColor}
+                  graphBarBackground={dataset.graphBarBackground}
+                />
+              </TableCell>
+              <TableCell>
+                <GraphPercentage 
+                  percentageData={dataset.percentage}
+                  borderColor={dataset.graphPercentageBorder}
+                  background={dataset.graphPercentageBackground}
+                  textColor={dataset.percentageTextColor}
+                />
+              </TableCell>
+            </TableRow>
           ))}
-        </table>
-      </div>
+        </TableBody>
+      </Table>
+    </TableContainer>
     </div>
+    
   );
 };
+
+export default AllProductsChart;
