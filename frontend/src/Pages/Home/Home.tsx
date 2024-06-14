bf88575694cec4aefda396f93fbb2d3ad199df9b
Elizabete <elizabete958@gmail.com>
2024-06-13 21:10:01 -0300

    refactor: pagina home primeira parte

--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -1,47 +1,64 @@
+import { useState } from 'react';
 
-import { LineChart } from "recharts";
-import { AllProductsChart } from "../../Components/Chart/AllProductsChart";
-import { PerformanceChart } from "../../Components/Chart/PerformanceChart";
-import { PlatFormChart } from "../../Components/Chart/PlatformChart";
-import { Card } from "../../Components/Chart/SalesChart";
-import { Chart } from "../../Components/Chart/LifeChart";
+import AllProductsChart from '../../Components/Chart/AllProductsChart';
+import LineChartTsx from '../../Components/Chart/LineChart';
+import { PerformanceChart } from '../../Components/Chart/PerformanceChart';
+import { PlatFormChart } from '../../Components/Chart/PlatformChart';
+import { Card } from '../../Components/Chart/SalesChart';
+import { NavAside } from '../../Components/NavAside';
+import './home.css';
 
 export function Home() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div className="md:grid h-screen md:grid-cols-[14rem_1fr] flex flex-col">
       <div className="flex flex-col md:flex-row w-screen">
         
-        <div className="flex w-screen flex-col">
-          {/* <Header /> */}
-          <main className="flex pt-16 p-5 flex-col gap-2">
-            <section className="mt-10 flex flex-col xl:flex-row gap-2 lg:ml-60">
-              <div className="flex-1 xl:w-[60%]">
-                <Card />
-              </div>
-              <div className="flex-1 xl:w-[40%]">
-                <PlatFormChart />
-              </div>
-            </section>
-            {/* Segundo container de cards */}
-            <section className="flex justify-start w-full lg:ml-60  mt-4">
-              <div className="w-full lg:w-auto">
-                <PerformanceChart />
-                <AllProductsChart />
-              </div>
-            </section>
-
-            {/* Terceiro container de cards */}
-            <section className="flex justify-start w-full lg:ml-60  mt-4">
-              <div className="w-full lg:w-auto">
-                <LineChart />
-                <Chart />
-              </div>
-            </section>
-          </main>
+        <div className="hidden md:flex w-full flex-col">
+          <NavAside closeSidebar={toggleSidebar} />
         </div>
+        
+        <main className="flex p-5 flex-col gap-2">
+          <section className="mobile flex flex-col xl:flex-row gap-2">
+            <div className="">
+              <Card />
+            </div>
+            <div className="">
+              <PlatFormChart />
+            </div>
+          </section>
+          
+          <section className="mobile flex flex-col xl:flex-row gap-2">
+            <div className="">
+              <PerformanceChart />
+            </div>
+            <div className="">
+              <LineChartTsx />
+            </div>
+            <div className="">
+              <LineChartTsx />
+            </div>
+          </section>
+
+          <section className="mobile flex flex-col xl:flex-row gap-2">
+            <div className="">
+              <AllProductsChart />
+            </div>
+            <div className="">
+              <LineChartTsx />
+            </div>
+            <div className="">
+              <LineChartTsx />
+            </div>
+          </section>
+
+        </main>
       </div>
     </div>
   );
 }
-
-export default Home;
